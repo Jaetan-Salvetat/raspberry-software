93b064ed549d51bd1b2eae962b9d09f285e7e2e8
Jaetan-Salvetat <gaetansalvi08@gmail.com>
2025-05-21 07:11:16 +0200

    test

--- setup.py
+++ setup.py
@@ -3,23 +3,37 @@ from cx_Freeze import setup, Executable
 
 build_exe_options = {
     "packages": [
-        "PySide6", 
         "PySide6.QtCore", 
         "PySide6.QtGui", 
         "PySide6.QtWidgets", 
         "PySide6.QtQml", 
-        "PySide6.QtQuick"
+        "PySide6.QtQuick",
+        "PySide6.QtQuick.Controls", 
+        "PySide6.QtQuick.Layouts", 
+        "PySide6.QtQuick.Window", 
+        "PySide6.QtQuick.Effects"
     ],
     "excludes": [
         "tkinter", 
         "unittest", 
         "http", 
-        "xml", 
         "pydoc",
+        "doctest",
+        "distutils",
+        "pip",
+        "setuptools",
         "PySide6.QtAsyncio",
         "PySide6.QtWebEngine",
         "PySide6.QtWebEngineCore",
-        "PySide6.QtWebEngineWidgets"
+        "PySide6.QtWebEngineWidgets",
+        "PySide6.QtSql",
+        "PySide6.QtTest",
+        "PySide6.Qt3DCore",
+        "PySide6.Qt3DInput",
+        "PySide6.Qt3DLogic",
+        "PySide6.Qt3DRender",
+        "PySide6.Qt3DAnimation",
+        "PySide6.Qt3DExtras"
     ],
     "include_files": [
         ("ui", "ui"),
@@ -27,10 +41,16 @@ build_exe_options = {
         ("version.py", "version.py"),
         ("update_checker.py", "update_checker.py")
     ],
+    "bin_excludes": [
+        "libstdc++.so.6",
+        "libgcc_s.so.1"
+    ],
     "bin_includes": [
         "libGL.so.1"
     ],
-    "include_msvcr": True
+    "zip_includes": [],
+    "include_msvcr": True,
+    "optimize": 2
 }
 
 executables = [
